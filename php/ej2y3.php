104fe2b70660c4d74c368ab8acadaa0c0756c34a
Marcos Vicente Amián <marcosvicente04@iesamchado.org>
2024-11-28 14:08:11 +0100

    clase

---
+++ php/ej2y3.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ejercicio 2 y 3 complementados. PHP</title>
+</head>
+<body>
+    <?php 
+    $cadena1 = "Hola mundo";
+    $cadena2 = " desde php";
+    echo $cadena1 . $cadena2;
+    print "<p>Este mensaje está escrito con la instruccion print</p>";
+    
+    ?>
+</body>
+</html>
